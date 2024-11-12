9b9776dca1a3fdd96937f93200c55a24baefbee0
Robbhedonic <rccstockholm@outlook.com>
2024-11-13 00:01:54 +0100

    Created base structure

---
+++ src/Pages/HomePage.tsx
@@ -0,0 +1,5 @@
+export const HomePage = () => {
+    return ( <h1>MovieFlix</h1>
+    )
+}
+export default HomePage
---
+++ src/Pages/MoviesPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MoviesPage = () => {
+  return <h1>Populära Filmer</h1>
+}
+
+export default MoviesPage
